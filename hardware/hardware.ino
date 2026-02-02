af99635e7ca12137764286a7731796d82ecf9d9e
Patrick Marsden <pmarsden2k5@gmail.com>
2026-02-02 18:56:46 -0500

    Final Lab 1 Fixes: Consolidated routes and fixed MQTT broker

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -55,8 +55,8 @@ static const char* mqtt_server    = "www.yanacreations.com";                // B
 static uint16_t mqtt_port         = 1883;
 
 // WIFI CREDENTIALS
-const char* ssid                  = "gadfa’s iPhone"; // Add your Wi-Fi ssid
-const char* password              = "12345678"; // Add your Wi-Fi password 
+const char* ssid                  = "MonaConnect"; // Add your Wi-Fi ssid
+const char* password              = ""; // Add your Wi-Fi password 
 
 
 
